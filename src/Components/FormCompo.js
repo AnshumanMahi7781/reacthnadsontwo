87e4ef8188a4a3303eeb9fd72c921240cc53d16d
Anshuman Kumar <mahianshuman491@gmail.com>
2023-10-05 01:09:51 +0530

    Layout and Form Validation Update

--- src/Components/FormCompo.js
+++ src/Components/FormCompo.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react'
+import React, { Component, createRef } from 'react'
 
 class FormCompo extends Component {
     constructor(props) {
@@ -7,8 +7,14 @@ class FormCompo extends Component {
             empName: "",
             empDepart: "",
             empRating: "",
-            empDatabase: []
+            empDatabase: [],
+            nameError: "",
+            departError: "",
+            ratingError: "",
         }
+        this.empNameRef = createRef();
+        this.empDepartRef = createRef();
+        this.empRatingRef = createRef();
     }
     changeHandler = (e) => {
         this.setState({
@@ -16,23 +22,71 @@ class FormCompo extends Component {
         });
     }
 
+    // Run whenever there is an error in input field  and after 2ssec clear the errr
+    removeError = () => {
+        setTimeout(() => {
+            this.setState({
+                nameError: "",
+                departError: "",
+                ratingError: "",
+            })
+        }, 2000);
+    }
+
+
     submitHandler = (e) => {
         e.preventDefault();
-        let tempObj = {
-            empName: this.state.empName,
-            empDepart: this.state.empDepart,
-            empRating: this.state.empRating
+
+        // Checking if the empName is empty
+        if (this.state.empName.length === 0 || !isNaN(this.state.empName)) {
+            this.setState({
+                nameError: "Fill Correct  Name"
+            })
+            this.removeError();
+            this.empNameRef.current.focus();
+            this.empNameRef.current.select();
         }
-        
-        if (this.state.empName.length !== 0 && this.state.empDepart.length !== 0 && this.state.empRating.length !== 0) {
+        // Checking if the empDepart is empty
+        else if (this.state.empDepart.length === 0) {
+            this.setState({
+                departError: "Fill Department Name"
+            })
+            this.removeError();
+            this.empDepartRef.current.focus();
+            this.empDepartRef.current.select();
+        }
+        // Checking if the empRating is empty or lest then 1 or greater then 5
+        else if (!this.state.empRating || parseInt(this.state.empRating) < 1 || parseInt(this.state.empRating) > 5) {
+            this.setState({
+                ratingError: "Give Rating Between 1 to 5"
+            })
+            this.removeError();
+            this.empRatingRef.current.focus();
+            this.empRatingRef.current.select();
+        }
+        // After here adding the data into database state and also clearning the state after savig the data
+        else {
+            let tempObj = {
+                empName: this.state.empName,
+                empDepart: this.state.empDepart,
+                empRating: this.state.empRating
+            }
             this.state.empDatabase.push(tempObj);
             this.setState({
                 empName: "",
                 empDepart: "",
                 empRating: "",
+                nameError: "",
+                departError: "",
+                ratingError: "",
                 empDatabase: this.state.empDatabase
             })
+            this.empNameRef.current.focus();
         }
+    }
+    // Call whenever the component is mounted to the DOm and give the focus to the empName Input
+    componentDidMount() {
+        this.empNameRef.current.focus();
     }
 
 
@@ -42,31 +96,57 @@ class FormCompo extends Component {
 
 
                 <form>
-                    <h1>EMPLOYEE FEEDBACK FORM</h1>
+                    <h2>EMPLOYEE FEEDBACK FORM</h2>
 
                     <div className="formRow">
                         <label htmlFor="empName">Name :</label>
-                        <input type="text" name='empName' id='empName' placeholder='Enter Name' onChange={this.changeHandler} value={this.state.empName} />
+                        <div className="inputBox">
+                            <input type="text" ref={this.empNameRef} name='empName' id='empName' placeholder='Enter Name' onChange={this.changeHandler} value={this.state.empName} />
+                            {this.state.nameError && <p className='Error'>{this.state.nameError}</p>}
+                        </div>
                     </div>
 
                     <div className="formRow">
                         <label htmlFor="empName">Department :</label>
-                        <input type="text" name='empDepart' id='empDepart' placeholder='Enter Departent' onChange={this.changeHandler} value={this.state.empDepart} />
+                        <div className="inputBox">
+                            <input type="text" ref={this.empDepartRef} name='empDepart' id='empDepart' placeholder='Enter Departent' onChange={this.changeHandler} value={this.state.empDepart} />
+                            {this.state.departError && <p className='Error'>{this.state.departError}</p>}
+                        </div>
                     </div>
 
                     <div className="formRow">
                         <label htmlFor="empName">Rating :</label>
-                        <input type="number" name='empRating' id='empRating' placeholder='Enter Rating' onChange={this.changeHandler} value={this.state.empRating} />
+                        <div className="inputBox">
+                            <input type="number" ref={this.empRatingRef} name='empRating' id='empRating' placeholder='Enter Rating' onChange={this.changeHandler} value={this.state.empRating} />
+                            {this.state.ratingError && <p className='Error'>{this.state.ratingError}</p>}
+                        </div>
                     </div>
                     <button onClick={this.submitHandler}>Submit</button>
                 </form>
 
                 <div className="dataContainer">
-                    {
-                        this.state.empDatabase.map((emp, index) => {
-                            return <p key={index} className='empData'>Name  : {emp.empName} | Department : {emp.empDepart} | Rating : {emp.empRating}</p>
-                        })
-                    }
+                    <table border={2}>
+                        <thead>
+                            <tr>
+                                <th>Name </th>
+                                <th>Department </th>
+                                <th>Rating</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {
+                                this.state.empDatabase.map((emp, index) => {
+                                    return (
+                                        <tr key={index}>
+                                            <td>{emp.empName}</td>
+                                            <td>{emp.empDepart}</td>
+                                            <td>{emp.empRating}</td>
+                                        </tr>
+                                    );
+                                })
+                            }
+                        </tbody>
+                    </table>
                 </div>
             </div>
         )
